2918bd2e4ffba6909f09bff65c4712a25ff369e2
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-10-11 19:14:26 +0200

    ez kimaradt

--- WEB191010/Controllers/UgyfelekController.cs
+++ WEB191010/Controllers/UgyfelekController.cs
@@ -10,33 +10,30 @@ namespace WEB191010.Controllers
     public class UgyfelekController : Controller
     {
         // GET: Ugyfelek
+        ApplicationDbContext _context;
+
+        public UgyfelekController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();    
+        }
+
         public ViewResult Index()
         {
-            var ufk = GetData();
+            var ufk = _context.Ugyfelek.ToList();
             return View(ufk);
         }
 
         public ActionResult Reszletek(int id)
         {
-            var uf = GetData().SingleOrDefault(x => x.Id == id);
+            var uf = _context.Ugyfelek.SingleOrDefault(x => x.Id == id);
 
             if (uf is null) return HttpNotFound();
             else return View(uf);
-        }
-
-        private IEnumerable<Ugyfel> GetData()
-        {
-            return new List<Ugyfel>
-            {
-                new Ugyfel() { Id = 1, Nev = "Bogdán Barnabás"},
-                new Ugyfel() { Id = 2, Nev = "Sándor Ramóna"},
-                new Ugyfel() { Id = 3, Nev = "Szabó Ákos"},
-                new Ugyfel() { Id = 4, Nev = "Bogdán Izabella"},
-                new Ugyfel() { Id = 5, Nev = "Rácz Ábel"},
-                new Ugyfel() { Id = 6, Nev = "Szűts Edina"},
-                new Ugyfel() { Id = 7, Nev = "Király Gábor"},
-                new Ugyfel() { Id = 8, Nev = "Oláh Kíra"},
-            };
         }
     }
 }
