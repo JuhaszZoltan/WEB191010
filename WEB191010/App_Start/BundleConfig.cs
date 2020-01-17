c2494d722e10916801d6021dcbb5017f82625c37
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-01-17 17:01:47 +0100

    becsajnak

--- WEB191010/App_Start/BundleConfig.cs
+++ WEB191010/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace WEB191010
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-journal.css",
